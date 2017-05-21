c1bac6f135568668ba7887b1d37a59f25d96f06f
SichYuriy <sichyuriyy@gmail.com>
2017-05-21 16:27:50 +0300

    first commit

---
+++ src/main/java/javatraining/spring/movies/Main.java
@@ -0,0 +1,11 @@
+package javatraining.spring.movies;
+
+/**
+ * Created by Yuriy on 5/21/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
